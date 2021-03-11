a8a787c9ad8e9c48f37f96f2f45552c38dda2036
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-03-11 23:40:44 +0330

    prob 520A accepted

--- 520.A/pangram.js
+++ 520.A/pangram.js
@@ -1,11 +1,5 @@
 var n = +readline();
-var word = readline().toLowerCase();
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-for(var i = 0; i < alphabet.length; i++){
-    if(word.indexOf(alphabet[i] === -1)){
-        print('NO');
-    } else {
-        print('YES');
-    }
-}
+var s = readline().toLowerCase();
+var sSet = new Set(s);
+ 
+print((sSet.size === 26)? 'YES':'NO');
