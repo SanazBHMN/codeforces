b70ada001c1e2e2159d8f11556aa6707c23d0e5f
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-03-16 14:23:37 +0330

    prob 1352A success in getting round nums

--- 1352.A/sumOfRoundNum.js
+++ 1352.A/sumOfRoundNum.js
@@ -1,17 +1,18 @@
 var t = +readline();
 
 for(var i = 0; i < t; i++){
-    var n = readline().split('');
+    var n = readline().split('').map(Number);
     var k = 0;
     for(var j = 0; j < n.length; j++){
-        if(n[j] !== '0'){
+        if(n[j] !== 0){
             k++;
+			print(n[j] * Math.pow(10, (n.length - j - 1)));
         }
     }
     print(k);
-    for(var k = 0; k < n.length; k++){
+    /*for(var k = 0; k < n.length; k++){
         if(n[k] !== '0'){
             print(n[k] + '0'.repeat(n.length - k - 1));
         }
-    }
+    }*/
 }
