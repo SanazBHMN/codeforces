cb75b31758ccb31f834da829c642f8974c5f3d66
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-04-20 22:31:22 +0430

    working on prob 1343b

---
+++ 1343.B/balanced array.js
@@ -0,0 +1,14 @@
+var t = +readline();
+var halfN = n / 2;
+var arr = [];
+
+for(var i = 0; i < t; i++) {
+    var n = +readline();
+    
+    if((n / 2) % 2 !== 0) print('NO');
+    else {
+        print('YES');
+        for(var j = 1; j < halfN; j++) {
+        }
+    }
+}
