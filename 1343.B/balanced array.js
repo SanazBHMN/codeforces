270fe5f22d3e2d33b16fee0f8d890dd1f0632f47
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-04-21 15:42:20 +0430

    working on prob 1343B

--- 1343.B/balanced array.js
+++ 1343.B/balanced array.js
@@ -8,7 +8,7 @@ for(var i = 0; i < t; i++) {
     if((n / 2) % 2 !== 0) print('NO');
     else {
         print('YES');
-        for(var j = 1; j < halfN; j++) {
+        for(var j = 0; j < halfN; j++) {
         }
     }
 }
