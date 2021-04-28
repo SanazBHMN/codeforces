174f8a4e806a2e8ec3551590cbeb9977642925c4
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-04-28 18:26:41 +0430

    prob 785A accepted

--- 282A.js
+++
@@ -1,9 +0,0 @@
-for(var i = 1; i <= 5; i++) {
-    for (var j = 1; j <= 5; j++) {
-        var input = readline().split('').map(Number);
-        if(input[i][j] === 1) {
-            print(abs(i - 3) + abs(j - 3));
-            
-        }
-    }
-}
