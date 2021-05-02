51df732aac1505f6fc316d2ea42e26b8014bbd4c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-05-02 22:13:40 +0430

    working on prob 490A

---
+++ 490.A/team olympiad.js
@@ -0,0 +1,31 @@
+var n = +readline();
+var t = readline().split(' ').map(Number);
+var t_1 = 0;
+var t_2 = 0;
+var t_3 = 0;
+var index1 = [];
+var index2 = [];
+var index3 = [];
+
+for(var i = 0; i < n; i++) {
+    if(t[i] === 1){
+        index1.push(i+1);
+        t_1++;
+    }else if(t[i] === 2){
+        index2.push(i+1);
+        t_2++;
+    }else {
+        index3.push(i+1);
+        t_3++;
+    }
+}
+print(index1);
+print(index2);
+print(index3);
+
+if(t_1 !== 0 && t_2 !== 0 && t_3 !== 0){
+    print(Math.min(t_1, t_2, t_3));
+    
+}else{
+    print(0);
+}
