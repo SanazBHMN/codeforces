bf21b393c65921046b660a3bd9b7db0f2f42bffb
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-06-26 22:47:34 +0430

    Solve prob 1372A

---
+++ 1372.A/omkar and completion.js
@@ -0,0 +1,10 @@
+var t = +readline();
+
+for (var i = 0; i < name; i++) {
+  var n = +readline();
+  var arr = [];
+  for (var j = 0; j < n; j++) {
+    arr.push(1);
+  }
+  print(arr.join(" "));
+}
