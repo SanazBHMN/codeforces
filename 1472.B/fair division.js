09557e675e86e77a461f5d7479d50f24da112094
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-21 20:44:01 +0430

    First approach on prob 1472B

--- 1472.B/fair division.js
+++ 1472.B/fair division.js
@@ -1,31 +1,27 @@
 var t = +readline();
 
-for(var i = 0; i < t; i++) {
-    var n = +readline();
-    var weights = readline().split(' ').map(Number);
-    var qnt_1 = 0;
-    var qnt_2 = 0;
-    
-    for(var j = 0; j < n; j++) {
-        if(weights[j] === 1) {
-            qnt_1++;
-        } else {
-            qnt_2++;
-        }
-    }
-    
-    if(qnt_2 !== 0) {
-        if(qnt_1 % 2 === 0 && qnt_1 > 0) {
-            print('YES');
-        } else {
-            print('NO');
-        }
+while (t--) {
+  var n = +readline();
+  var weights = readline().split(" ").map(Number);
+  var sum_weights = weights.reduce((acc, item) => acc + item);
+  var ones = 0;
+  var twos = 0;
+  var sum = weights[0];
+
+  for (var i = 0; i < n; i++) {
+    if (weights[i] === 1) {
+      ones++;
     } else {
-        if(qnt_1 % 2 === 0) {
-            print('YES');
-        } else {
-            print('NO');
-        }
+      twos++;
     }
-    
+  }
+  if (ones % 2) {
+    print("NO");
+    continue;
+  }
+  if (sum_weights % 4 && ones === 0) {
+    print("NO");
+    continue;
+  }
+  print("YES");
 }
