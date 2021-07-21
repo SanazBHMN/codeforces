4a78540ff37802b7aa65e522d7f223bba90b5876
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-21 21:19:37 +0430

    Ternary operator instead of if

--- 1353.A/most ustable array.js
+++ 1353.A/most ustable array.js
@@ -1,11 +1,9 @@
 var t = +readline();
 
 while (t--) {
-  var input = readline().split(" ").map(Number);
+  var input = readline().split(' ').map(Number);
   var n = input[0];
   var m = input[1];
-
-  if (n === 1) print(0);
-  else if (n === 2) print(m);
-  else print(2 * m);
+  
+  print((n === 1) ? 0 : (n === 2) ? m : 2 * m);
 }
