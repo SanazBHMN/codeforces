f6a21e4a3fe3b698e011bf591b1d1c0535659914
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-28 20:50:56 +0430

    First approach

---
+++ 1520/b.js
@@ -0,0 +1,16 @@
+var t = +readline();
+
+while (t--) {
+  var n = +readline();
+  var d = Math.floor(Math.log10(n)) + 1;
+  var o = (d - 1) * 9; // ordinary numbers with less number of digits than n
+  var firstOrdinary = (Math.pow(10, d) - 1) / 9;
+  
+  for(var i = 1; i < 10; i++) {
+      if(n < firstOrdinary * i) {
+          break;
+      }
+      o++;
+  }
+  print(o);
+}
