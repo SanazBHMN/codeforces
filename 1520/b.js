94e63c060f0f11f3cd501e1446f842da15a01424
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-28 20:52:51 +0430

    Second approach

--- 1520/b.js
+++ 1520/b.js
@@ -5,12 +5,13 @@ while (t--) {
   var d = Math.floor(Math.log10(n)) + 1;
   var o = (d - 1) * 9; // ordinary numbers with less number of digits than n
   var firstOrdinary = (Math.pow(10, d) - 1) / 9;
-  
-  for(var i = 1; i < 10; i++) {
-      if(n < firstOrdinary * i) {
-          break;
-      }
-      o++;
-  }
+
+  // for(var i = 1; i < 10; i++) {
+  //       if(n < firstOrdinary * i) {
+  //           break;
+  //       }
+  //       o++;
+  //   }
+  for (var i = 1; i < 10 && n >= firstOrdinary * i; i++, o++);
   print(o);
 }
